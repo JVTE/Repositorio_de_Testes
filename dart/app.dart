3b62ae7bd11932b567987d9ec1dbc2689e7f96a4
João Vitor Torresan <56739230+JVTE@users.noreply.github.com>
2023-06-02 09:25:34 -0300

    a

---
+++ dart/app.dart
@@ -0,0 +1,22 @@
+void main(){
+  duble media = 4.9;
+
+  if (media < 6.0){
+    print("Reprovado!")
+  } else {
+    print("Aprovado!")
+  }
+}
+
+print(media < 6.0 ? "Reprovado!" : "Aprovado!");
+
+String linguagem;
+print(linguagem ?? "Não Informado");
+
+linguagem = "Dart";
+print(linguagem ?? "Não Informado");
+
+switch(linguagem){
+  case "Dart";
+  print
+}
